2918a0a8a3ce2ae8af85ddd0cfae0316d722965a
gk1402-cacto <truongduyhailowgo330@gmail.com>
2025-12-03 11:30:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,5 @@ def webhook():
             send_message(chat_id, f"Bạn gửi: {text}")
 
     return jsonify(success=True)
-
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
